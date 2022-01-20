ef560152ef02a2a65db8c10a10f6a26437ff03f5
Kostya <leap74rus@gmail.com>
2022-01-20 16:46:43 +0300

    make it better

---
+++ main.py
@@ -0,0 +1,41 @@
+import logging
+
+from aiogram import Bot, Dispatcher, executor, types
+from pyowm import OWM
+
+from pyowm.utils.config import get_default_config
+
+owm = OWM('e2ca9ad78754497305dfde6271388a60')
+mgr = owm.weather_manager()
+config_dict = get_default_config()
+config_dict['language'] = 'ru'
+
+
+API_TOKEN = '2103674705:AAFbj-5Ty-8yiElQZ1rsIAHx1M4NCDRvuno'
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(content_types=['text'])
+async def send_echo(message: types.Message):
+    observation = mgr.weather_at_place(message.text)
+    w = observation.weather
+    temp = w.temperature('celsius')['temp']
+    status = w.detailed_status
+    await message.reply('В городе ' + message.text + ' сейчас ' + status + '\n')
+    await message.answer('Температура сейчас в районе ' + str(temp) + '\n\n')
+    if temp < 10:
+        await message.answer('Сейчас ппц как холодно, одевайся как танк!')
+    elif temp < 20:
+        await message.answer('Сейчас холодно оденься потеплее.')
+    else:
+        await message.answer('Температура норм, одевай, что угодно.')
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
